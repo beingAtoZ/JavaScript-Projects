7a0ef40123b0b1c1779627453bd8145689ac99f4
Afraz Arif <165937996+beingAtoZ@users.noreply.github.com>
2025-02-04 21:34:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+  alert("Welcome to the Guessing Game, Dumb Edition!");
+  alert("Enter q to surrender!");
+  while (true) {
+    let maximumNum = parseInt(
+      prompt(
+        "Oi! Enter a maximum number, and make sure it's actually a number:"
+      )
+    );
+    if (!maximumNum) {
+      // if the user inputs an invalid choice, the value of maximumNum would be falsy.
+      alert(
+        "Are you dense? I said a NUMBER, not whatever nonsense you just typed!"
+      );
+    } else {
+      let chosenNum = Math.floor(Math.random() * maximumNum + 1);
+      let guess = prompt("Take a wild guess (try not to embarrass yourself):");
+      let counter = 1;
+      if (guess.toLowerCase() == "q") {
+        alert("Quit already? LOSER!");
+        break;
+      }
+      while (chosenNum != parseInt(guess)) {
+        counter++;
+        if (parseInt(guess) < chosenNum) {
+          alert("Wow, that's pathetic. Try thinking bigger, genius.");
+        } else if (parseInt(guess) > chosenNum) {
+          alert("Whoa, slow down there Einstein, go lower.");
+        } else if (guess.toLowerCase() == "q") {
+          alert("Quit already? LOSER!");
+          break;
+        } else {
+          alert("Wow, how dumb a person can be! A NUMBER!");
+        }
+        guess = prompt("Guess again, if your tiny brain can handle it:");
+      }
+      if (parseInt(guess) == chosenNum) {
+        alert(
+          `Finally! Took you ${counter} tries to get it right. Took you long enough.`
+        );
+        break;
+      }
+    }
+    break;
+  }
